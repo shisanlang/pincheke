c39e5a6cc506952016f135239f248d8c2d3e46bc
shisanlang <duanxiaodong@localhost.(none)>
2013-07-30 00:01:45 +0800

    create project

---
+++ pincheke/pincheke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pincheke
+//
+//  Created by duan on 13-7-30.
+//  Copyright (c) 2013年 duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
