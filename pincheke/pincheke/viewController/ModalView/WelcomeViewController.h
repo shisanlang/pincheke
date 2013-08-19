04de4a90cff94c4396a6848441f00089029a8b72
shisanlang <shisanlangd@gmail.com>
2013-08-19 17:57:10 +0800

    test2

---
+++ pincheke/pincheke/viewController/ModalView/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  pincheke
+//
+//  Created by duan on 13-8-6.
+//  Copyright (c) 2013年 duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
---
+++ pincheke/pincheke/viewController/ModalView/WelcomeViewController.m
@@ -0,0 +1,38 @@
+//
+//  WelcomeViewController.m
+//  pincheke
+//
+//  Created by duan on 13-8-6.
+//  Copyright (c) 2013年 duan. All rights reserved.
+//
+
+#import "WelcomeViewController.h"
+
+@interface WelcomeViewController ()
+
+@end
+
+@implementation WelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
